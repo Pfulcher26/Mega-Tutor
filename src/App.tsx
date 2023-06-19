d34fe585e4b55d06e9355502ca125524f0093fbf
Pfulcher26 <pfulcher26@gamil.com>
2023-06-19 10:30:54 -0700

    Removes padding from rooter container and styles navbar.

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,23 @@ import { Container } from "react-bootstrap"
 import { Home } from './pages/Home'
 import { Appointments } from './pages/Appointments'
 import { About } from './pages/About'
+import { Navbar } from './components/Navbar'
 
 import './App.css'
 
 function App() {
 
   return (
-    <Container className="mb-4">
-      <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/appointments' element={<Appointments />}></Route>
-        <Route path='/about' element={<About />}></Route>
-      </Routes>
-    </Container>
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/appointments' element={<Appointments />}></Route>
+          <Route path='/about' element={<About />}></Route>
+        </Routes>
+      </Container>
+    </>
   )
 }
 
