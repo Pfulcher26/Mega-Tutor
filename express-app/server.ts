c8c6cc556e47591033be91d8dee7f10cb7c3e8f3
Pfulcher26 <pfulcher26@gamil.com>
2023-06-23 14:25:31 -0700

    Creats a test fetch request from react front-end to express back-end.  Also imports and enables CORS on server.ts

--- express-app/server.ts
+++ express-app/server.ts
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors')
 
 const app = express();
+
+//Enable CORS
+app.use(cors());
 
 // Serve static files from React build output
 app.use(express.static(path.join(__dirname, '../dist')));
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,25 @@
+import React, { useEffect, useState } from 'react';
 
+interface ApiResponse {
+  message: string;
+}
 
 export function About() {
-  return (
-    <div>About</div>
-  )
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:5001/api/data');
+      const responseData: ApiResponse = await response.json();
+      setMessage(prevMessage => prevMessage + responseData.message);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return <div>About: {message}</div>;
 }
